c6d07060dbe965219537e69986a84a3bfdd15f68
vishi Iyer <vishi@vishis-MacBook-Pro.local>
2018-10-08 13:42:08 -0700

    First Commit

---
+++ devops01a.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+# Bash Script Assigment 00
+
+Echo "         "
+Echo "****** Disk Usage *******"
+df -h
+Echo "         "
+
+
+Echo "****** CPU Information  *******"
+system_profiler SPHardwareDataType | grep 'Proces\|Core' |grep -v L2
+Echo "         "
